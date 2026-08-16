dc5626695fbee242d21adf75817c248d1b24393a
Husainiaza <148662620+Husainiaza@users.noreply.github.com>
2026-08-16 08:44:45 +0800

    Create wifi.ino

---
+++ DMD/wifi.ino
@@ -0,0 +1,48 @@
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  drawText(" Connected");
+
+  delay(5000);
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  message[length] = 0;
+  MessageTemp  = String(( char *) message);
+  delay(100);
+  Serial.println(MessageTemp);
+  
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("IoTMesejBoxlAza")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
